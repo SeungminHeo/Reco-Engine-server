bfd98711988a1a41908b5126bcf10a519c987b94
SEUNGMIN <hawi75@yonsei.ac.kr>
2020-11-25 05:45:48 +0900

    Init Spring project

---
+++ src/main/java/com/jolp/reco_server/RecoServerApplication.java
@@ -0,0 +1,13 @@
+package com.jolp.reco_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecoServerApplication.class, args);
+    }
+
+}
